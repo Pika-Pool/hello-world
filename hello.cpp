fd2a5487422fc3219b264b1f5aeae2e3420fe2b7
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-07-06 14:24:42 +0530

    hello.cpp - list command line arguments

--- hello.cpp
+++ hello.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int main() {
+int main(int argc, char *argv[])
+{
 	std::cout << "Hello World\n";
-	
+
+	std::cout << "Your arguments\n";
+	for (int i = 0; i < argc; ++i)
+	{
+		std::cout << i << ".\t" << argv[i] << '\n';
+	}
+
 	return 0;
 }
